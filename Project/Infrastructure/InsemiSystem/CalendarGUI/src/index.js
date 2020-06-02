37ca6da2d595045fdb3967030b581ba6092db049
arnyboy101 <49385664+arnyboy101@users.noreply.github.com>
2020-06-02 19:48:17 +0530

    Redux-Enabled Calendar (Arny Commit #15)

--- Project/Infrastructure/InsemiSystem/CalendarGUI/src/index.js
+++ Project/Infrastructure/InsemiSystem/CalendarGUI/src/index.js
@@ -1,2 +1,36 @@
-import App from "./components/App";
-import './components/app.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import MyApp from './Calendar';
+import * as serviceWorker from './serviceWorker';
+import "./App.css"
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+
+function reducer(state, action) {
+  console.log('reducer', state, action);
+  return state;
+  }
+  
+
+
+const store = createStore(reducer, applyMiddleware(thunk));
+const App = () => (
+<div>
+<MyApp />
+
+</div>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.register();
