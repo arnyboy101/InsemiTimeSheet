a47ea5b3a44ad21cfa4b803495f5703df5ffde3d
Panda5130 <panda5130@github.com>
2020-06-07 19:28:41 +0530

    #1 DB integration

--- Project/Infrastructure/InsemiSystem/InsemiSystem/settings.py
+++ Project/Infrastructure/InsemiSystem/InsemiSystem/settings.py
@@ -86,8 +86,12 @@ WSGI_APPLICATION = 'InsemiSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'TimeSheetDB',
+        'USER': 'admin',
+        'PASSWORD':'admin123',
+        'HOST': 'insemidb.cvyzrmqcspo0.ap-south-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
