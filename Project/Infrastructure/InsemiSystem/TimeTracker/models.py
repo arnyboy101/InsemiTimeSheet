7ea7e7f7e35442e5882b3d82321e523da26c6003
Panda5130 <panda5130@github.com>
2020-06-07 13:28:09 +0530

    TimeTracker Style Update

--- Project/Infrastructure/InsemiSystem/TimeTracker/models.py
+++ Project/Infrastructure/InsemiSystem/TimeTracker/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Activity(models.Model):
     CHOICES = [('1','Placeholder'),('2','While'),('3','We'),('4','Figure'),('5','This'),('6','Out')]
-    Project = models.CharField(max_length = 200, choices = CHOICES)
-    AddComments = models.TextField(max_length = 1000)
+    projects = models.CharField(max_length = 200, choices = CHOICES)
+    add_comments = models.TextField(max_length = 1000)
 
 
