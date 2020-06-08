d88028a600847331475b10a3f8a325f49983c3db
arnyboy101 <arnavsam@gmail.com>
2020-06-08 19:06:56 +0530

    Revert "Updated Models for TimeTracker (Arny Commit #23)"
    This reverts commit cc18ce9c4362c44f16e06b4e86d1de906c746d56.

--- Project/Infrastructure/InsemiSystem/InsemiSystem/settings.py
+++ Project/Infrastructure/InsemiSystem/InsemiSystem/settings.py
@@ -86,12 +86,8 @@ WSGI_APPLICATION = 'InsemiSystem.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'TimeSheetDB',
-        'USER': 'admin',
-        'PASSWORD': 'admin123',
-        'HOST': 'insemidb.cvyzrmqcspo0.ap-south-1.rds.amazonaws.com',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
