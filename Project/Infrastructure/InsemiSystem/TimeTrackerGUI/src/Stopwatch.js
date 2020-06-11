3a3d4fbe2e3c11e51d4616f5458d1f30efa35ded
arnyboy101 <49385664+arnyboy101@users.noreply.github.com>
2020-06-12 00:22:26 +0530

    Revert "Homescreen :)"

--- Project/Infrastructure/InsemiSystem/TimeTrackerGUI/src/Stopwatch.js
+++ Project/Infrastructure/InsemiSystem/TimeTrackerGUI/src/Stopwatch.js
@@ -1,57 +1,65 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { start, stop, reset, tick } from './actions';
-
+const formattedSeconds = (sec) => 
+  ('0'+Math.floor(sec/3600)).slice(-2) +
+  ':' +
+  ('0'+ Math.floor(sec/60)%60 ).slice(-2) +
+  ':' +
+  ('0' + sec % 60).slice(-2)
   
-  let interval = null;
+  
 
 class Stopwatch extends React.Component {
   constructor(props) {
     super(props);
-    
+    this.state = { 
+      secondsElapsed: 0, 
+      lastClearedIncrementer: null
+    };
+    this.incrementer = null;
   }  
     
-  handleStartClick = () => {
-    
-    this.props.start();
-    interval = setInterval(() => {
-      if(this.props.isRunning){
-        this.props.tick();
-      }
-    },1000);
-    
+  handleStartClick() {
+    this.incrementer = setInterval( () =>
+      this.setState({
+        secondsElapsed: this.state.secondsElapsed + 1
+      })
+    , 1000);
   }
   
-  handleStopClick = () => {
-    interval = clearInterval(interval);
+  handleStopClick() {
+    clearInterval(this.incrementer);
+    this.setState({
+      lastClearedIncrementer: this.incrementer
+    });
   }
   
-  handleResetClick = () =>{
-
-    this.props.reset();
+  handleResetClick() {
+    clearInterval(this.incrementer);
+    this.setState({
+      secondsElapsed: 0,
+      laps: []
+    });
   }
-
-
-  
   
   render() {
     return (
       <div className="stopwatch">
-        <h1 className="stopwatch-timer">{this.props.formattedSecs}</h1>
-        
+        <h1 className="stopwatch-timer">{formattedSeconds(this.state.secondsElapsed)}</h1>
    
+        {(this.state.secondsElapsed === 0 ||
+          this.incrementer === this.state.lastClearedIncrementer
+          ? <Button className="start-btn" onClick={this.handleStartClick.bind(this)}><b>START</b></Button>
+          : <Button className="stop-btn" onClick={this.handleStopClick.bind(this)}><b>STOP</b></Button>
+        )}
         
-          <Button className="start-btn" onClick={this.handleStartClick}><b>START</b></Button>
-           <Button className="stop-btn" onClick={this.handleStopClick}><b>STOP</b></Button>
-        
-        
 
 
 
-
-          <Button className='reset-btn' onClick={this.handleResetClick}><b>RESET</b></Button>
-          
-      
+        {(this.state.secondsElapsed !== 0 &&
+          this.incrementer === this.state.lastClearedIncrementer
+          ? <Button className='reset-btn' onClick={this.handleResetClick.bind(this)}><b>RESET</b></Button>
+          : null
+        )}
 
         
       </div>
@@ -59,21 +67,6 @@ class Stopwatch extends React.Component {
   }
 }
 
-function mapStateToProps(state){
-  return {
-    secondsElapsed:state.secondsElapsed,
-    lastClearedIncrementer:state.lastClearedIncrementer,
-    formattedSecs:state.formattedSecs,
-    isRunning:state.isRunning,
-  };
-}
-
-const mapDispatchtoProps = {
-  start,
-  stop,
-  reset,
-  tick,
-}
 /** verbose component before 0.14
 class Button extends React.Component {
   render() {
@@ -86,4 +79,4 @@ class Button extends React.Component {
 const Button = (props) =>
   <button type="button" {...props} className={"btn " + props.className } />;
 
-export default connect(mapStateToProps,mapDispatchtoProps)(Stopwatch);
+export default Stopwatch;
--- Project/Infrastructure/InsemiSystem/TimeTrackerGUI/src/index.js
+++ Project/Infrastructure/InsemiSystem/TimeTrackerGUI/src/index.js
@@ -6,65 +6,22 @@ import "./Stopwatch.css";
 import "./TTForm.css";
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { Provider } from 'react-redux';
 
 
-const Initial_Stopwatch_State = {
-  secondsElapsed: 0, 
-  isRunning: false,
-  formattedSecs: "",
-
-};
-const incrementer = null;
-const formattedSeconds = (sec) => 
-  ('0'+Math.floor(sec/3600)).slice(-2) +
-  ':' +
-  ('0'+ Math.floor(sec/60)%60 ).slice(-2) +
-  ':' +
-  ('0' + sec % 60).slice(-2)
-
-function Stopwatch_reducer(state=Initial_Stopwatch_State, action) {
+function reducer(state, action) {
   console.log('reducer', state, action);
-  switch(action.type){
-    case 'START':
-      return {
-        isRunning:true,
-        secondsElapsed: 0,  
-      };
-    case 'TICK':
-      return{
-        secondsElapsed: state.secondsElapsed+1,
-       formattedSecs: formattedSeconds(state.secondsElapsed)
-      };
-
-    case 'STOP':
-      return {
-      };
-    case 'RESET':
-      return {
-        isRunning:false,
-        secondsElapsed: 0
-      };
-    default:
-      return state;
-    
-    
-  }
-  
+  return state;
   }
   
 
 
-
+const store = createStore(reducer, applyMiddleware(thunk));
 const App = () => (
-<Provider store = {store}>
 <div>
 <Stopwatch />
+<TTForm />
 </div>
-</Provider>
 );
-
-const store = createStore(Stopwatch_reducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
