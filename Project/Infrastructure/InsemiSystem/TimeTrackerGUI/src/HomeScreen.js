c402f7bf197927f2712f90403495f4a598bfb76c
arnyboy101 <arnavsam@gmail.com>
2020-06-15 13:33:52 +0530

    Comments Added to HomeScreen GUI (Arny Commit #39)

--- Project/Infrastructure/InsemiSystem/TimeTrackerGUI/src/HomeScreen.js
+++ Project/Infrastructure/InsemiSystem/TimeTrackerGUI/src/HomeScreen.js
@@ -24,7 +24,7 @@ class HomeScreen extends React.Component {
             comments_value:"",
             loggedtime:0,
             loggedtime2:0,
-            data:[{bruh:0}],
+            data:[{place_holder:0}],
             placeholder:"",            
             loaded:false,
             runCount:0
@@ -32,9 +32,12 @@ class HomeScreen extends React.Component {
         this.incrementer = null;
     }
 
+    //Send to Database Button 
     onStopConfirmed = ()=>{
             let stop = confirm('Are you sure you want to submit?');
+            //If the user clicks OK
             if(stop==true){
+              //Posts the data into the database in the below format
             let data2 = {
                 employeeId:0,
                 Project:this.state.user_Selection,
@@ -60,7 +63,7 @@ class HomeScreen extends React.Component {
 
 
    
-        
+    //Fetches choices - Parsing by HLU needs to be implemented   
     componentDidMount() {
         fetch("/timetracker/api/Choices/")
         .then(response => {
@@ -83,7 +86,7 @@ class HomeScreen extends React.Component {
         console.log("Data - "+this.state.data);
     }
     
-      
+      //Handles STOP button
       handleStopClick() {
         let stop = confirm("You are about to stop this project");
         if (stop==true){
@@ -95,22 +98,16 @@ class HomeScreen extends React.Component {
           lastClearedIncrementer: this.incrementer,
           secondsElapsed: 0
         });
-
+        //Activates Send Database Panel
         stopped = true;   
 
         }
       }
-
-    logged_time_checker(){
-      console.log(this.state.loggedtime)
-    }
-      
-
+      //Handles the 'Start Tracking' button
      HandleSubmit = (event) => {
       
         event.preventDefault();
         if(confirm("You are selecting " + this.state.drop_down_value + " Project")){
-          console.log('are you evem workuvnjkvn');
         this.setState({user_Selection:this.state.drop_down_value, add_Comments:this.state.comments_value});
         this.incrementer = setInterval( () =>
           this.setState({
@@ -119,44 +116,38 @@ class HomeScreen extends React.Component {
         , 1000);
         }
       }
+    //Detects change in state of drop-down, temporary storage + editable
     HandleDropDown = (event) => {
         this.setState({drop_down_value: event.target.value});
       }
+    ////Detects change in state of additional comments, temporary storage + editable
      HandleComments = (event) => {
        this.setState({comments_value:event.target.value})
      }
-
-     //I'm not changing any actual code, just adding comments to test workflow
-     //Same thing again
-     //Here we go again!
-     //I Really hope it works this time
 
      render(){
          return(
     <div className = 'AppBoi'>
         <div className="stopwatch">
             <h1 className="stopwatch-timer">{formattedSeconds(this.state.secondsElapsed)}</h1>
-           
             <Button className="btn stop-btn" onClick={this.handleStopClick.bind(this)}><b>STOP</b></Button>
-        
-           
-            
-            
-                       
         </div>
         <div className="TTForm">
                 <div className='TTFORMDATA'>
                     <form onSubmit={this.HandleSubmit}>
                     
                         <span>Select Your Project:</span>
-                        
+                        //Gets choices API data
                         {this.state.data.map(choiceList => {
+                          //Stores tthe choices in local variable
                             let choices1 = choiceList.choices;
+                            //Only runs if choices are fetched
                             if (choices1!=null)
                             {
+                                //Splits words into an array and removes the '[]'
                                 choices1 = choices1.slice(1,-1).split(",");
                                 const items = []
-
+                                //iterates through choices1 and stores each choice as a key-value pair within an option
                                 for (const [index, value] of choices1.entries()) {
                                   items.push(<option key={index} value={value}>{value}</option>);
                                 }
@@ -183,6 +174,9 @@ class HomeScreen extends React.Component {
                         
                     </form>
                     </div>
+                    {
+                      //Displays send to database only if 'stopped has been set to true'
+                    }
                     {(stopped?
                     <div className = 'SubConfirmation'>
                       <p>Submit - </p>
