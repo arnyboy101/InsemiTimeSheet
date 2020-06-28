78a8005c26465cea05f21805100bf722e39e2ac7
Panda5130 <panda5130@github.com>
2020-06-28 20:56:48 +0530

    HomeScreen CSS (Shivam#2)

--- Project/Infrastructure/InsemiSystem/HomeScreenGUI/src/index.js
+++ Project/Infrastructure/InsemiSystem/HomeScreenGUI/src/index.js
@@ -1,1 +1,27 @@
+<<<<<<< Updated upstream
 import App from "./components/App";
+=======
+import React from 'react';
+import ReactDOM from 'react-dom';
+import HomeScreen from './HomeScreen';
+
+
+
+
+
+
+
+
+const App = () => (
+<div>
+<HomeScreen />
+</div>
+);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
+>>>>>>> Stashed changes
