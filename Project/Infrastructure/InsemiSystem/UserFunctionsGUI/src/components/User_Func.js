8417088fd56681c9e0b8296252cd5afcc8c08817
LohitReddy359 <kamatham.lohit@gmail.com>
2020-07-07 19:35:20 +0530

    Permission 1: User Functions Completed (Lohit and Sid Commit #4)

--- Project/Infrastructure/InsemiSystem/UserFunctionsGUI/src/components/User_Func.js
+++ Project/Infrastructure/InsemiSystem/UserFunctionsGUI/src/components/User_Func.js
@@ -2,119 +2,39 @@ import React from 'react';
 import Cookies from 'js-cookie'
 const Button = (props) =>
   <button type="button" {...props} className={ props.className } />;
-const formattedSeconds = (sec) => 
-  ('0'+Math.floor(sec/3600)).slice(-2) +
-  ':' +
-  ('0'+ Math.floor(sec/60)%60).slice(-2) +
-  ':' +
-  ('0' + sec%60).slice(-2)
-let change = false;  
+
 class User_Func extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            secondsElapsed :0,
-            lastClearedIncrementer: null,
-            user_Selection:"",
-            drop_down_value:"",
-            add_Comments:"",
-            comments_value:"",
-            loggedtime:0,
-            loggedtime2:0,
-            data:[{place_holder:0}],
-            placeholder:"",            
-            loaded:false,
-            runCount:0
+            change: false,
+            delete: false
         };
         this.incrementer = null;
     }
-    //Send to Database Button 
-    onStopConfirmed = ()=>{
-            let stop = confirm('Are you sure you want to submit?');
-            //If the user clicks OK
-            if(stop==true){
-              //Posts the data into the database in the below format
-            let data2 = {
-                employeeId:0,
-                Project:this.state.user_Selection,
-                AddComments:this.state.add_Comments,
-                logged_time:this.state.loggedtime
-            };
-            let csrftoken = Cookies.get('csrftoken');
-            fetch('/timetracker/api/TimeTracker/allObjects/',{
-                method: 'POST',
-                headers:{'Content-type':'application/json','X-CSRFToken':csrftoken},
-                body:JSON.stringify(data2)
-            }).then(response => {
-                if (response.status > 400) {
-                  return this.setState(() => {
-                    return { placeholder: "Something went wrong!" };
-                  });
-                }
-                return response.json();
-              })
-            }
-        }
-    //Fetches choices - Parsing by HLU needs to be implemented   
-    componentDidMount() {
-        fetch("/timetracker/api/Choices/")
-        .then(response => {
-          if (response.status > 400) {
-            return this.setState(() => {
-              return { placeholder: "Something went wrong!" };
-            });
-          }
-          return response.json();
-        })
-        .then(data => {
-          this.setState(() => {
-            return {
-              data,
-              loaded: true
-            };
-          });
-        });
-        console.log("Data - "+this.state.data);
-    }
-      //Handles STOP button
-      handleStopClick() {
-        let stop = confirm("You are about to stop this project");
-        if (stop==true){
-        clearInterval(this.incrementer);
-        this.setState({
-          loggedtime:this.state.secondsElapsed,
-          lastClearedIncrementer: this.incrementer,
-          secondsElapsed: 0
-        });
-        //Activates Send Database Panel
-        stopped = true;   
-        }
-      }
-      //Handles the 'Start Tracking' button
-     HandleSubmit = (event) => {
-        event.preventDefault();
-        if(confirm("You are selecting " + this.state.drop_down_value + " Project")){
-        this.setState({user_Selection:this.state.drop_down_value, add_Comments:this.state.comments_value});
-        this.incrementer = setInterval( () =>
-          this.setState({
-            secondsElapsed: this.state.secondsElapsed + 1
-          })
-        , 1000);
-        }
-      }
-    //Detects change in state of drop-down, temporary storage + editable
-    HandleDropDown = (event) => {
-        this.setState({drop_down_value: event.target.value});
-      }
-    ////Detects change in state of additional comments, temporary storage + editable
-     HandleComments = (event) => {
-       this.setState({comments_value:event.target.value})
-     }
-    UserChange() {
-        let stop = confirm("You are about to stop this project");
-        change = true;   
+    
+    UserChangeButton() {
+        this.setState({change: true}); 
+        this.setState({delete: false}); 
         
-  }
+    }
+    UserDeleteButton() {
+        this.setState({delete: true}); 
+        this.setState({change: false});
+        
+    }
+    UserDelete() {
+        this.setState({delete: false}); 
+        this.setState({change: false}); 
+        
+    }
+    UserChange() {
+        this.setState({change: false}); 
+        this.setState({delete: false}); 
+        
+    }
+
+
 
      render(){
          return(
@@ -125,17 +45,14 @@ class User_Func extends React.Component {
         <div className = "User_Buttons">
         <a href="/users/register/" target="_parent"> <button className = "User_Create">Create User</button></a>
         <br/>
-        <Button className="User_Change" onClick={this.UserChange.bind(this)}><b>Change User Details</b></Button>
+        <Button className="User_Change" onClick={this.UserChangeButton.bind(this)}>Change User Details</Button>
         <br/>
-        <a href="/users/delete/" target="_parent"><button className = "User_Delete">Delete User</button></a> 
+        <Button className="User_Delete" onClick={this.UserDeleteButton.bind(this)}>Delete User</Button> 
 
           <div className = "Exit">
               <a href="/home/" target="_parent"> <button className = "Exit">Go back to homescreen</button></a>
           </div>
-                    {
-                      //Displays send to database only if 'stopped has been set to true'
-                    }
-                    {(change?
+                    {(this.state.change?
                     <div>
                 <p>Edit fields required, leave others blank</p>
                 <div>
@@ -153,8 +70,19 @@ class User_Func extends React.Component {
                         <option key="Employee">Employee</option>
                     </select>
                 </div>
-                  <button onclick="">Make Changes</button>
+                  <Button className="Make_Changes" onClick={this.UserChange.bind(this)}>Make Changes</Button>
                 </div>
+                    :
+                    <div></div>
+                    )}
+                    {(this.state.delete?
+                    <div>
+                        <p>Search for Employee by ID</p>
+                        <input type="number"></input>
+                        <button onClick="">Search</button>
+                        <br/>
+                        <Button className="Delete" onClick={this.UserDelete.bind(this)}>Delete from Database</Button>
+                    </div>
                     :
                     <div></div>
                     )}
