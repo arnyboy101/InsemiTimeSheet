0d61ac5bedacffd846cc0112219197236efc4a8d
LohitReddy359 <kamatham.lohit@gmail.com>
2020-08-07 20:13:59 +0530

    Lohit and Sid Commit #10

--- Project/Infrastructure/InsemiSystem/Time/models.py
+++ Project/Infrastructure/InsemiSystem/Time/models.py
@@ -6,16 +6,8 @@ from django_mysql.models import ListCharField
 status_choices = ["WFH","WFO","Earned Leave","Sick Leave","Maternity Leave","Paternity Leave","Emergency Leave"]
 class Activity(models.Model):
     employeeId = models.CharField(max_length = 64, default=None)
-    employeeName = models.CharField(max_length = 256, default=None)
     Project_code = models.CharField(max_length = 256, default=None)
-    Project_name = models.CharField(max_length = 256, default=None)
-    Manager = models.CharField(max_length = 256, default=None)
-    Vendor_name = models.CharField(max_length = 256, default=None)
-    Leaves_availed = models.IntegerField
-    Work_location = models.CharField(max_length = 256, default=None)
-    Customer_name = models.CharField(max_length = 256, default=None)
     Date = models.DateField(auto_now_add = False, default=None)
-    Day = models.CharField(max_length = 16, default=None)
     Opening_time = models.TimeField(auto_now_add=False, default=None)
     Closing_time = models.TimeField(auto_now_add=False, default=None)
     Total_hours = models.TimeField(auto_now_add=False, default=None)
--- Project/Infrastructure/InsemiSystem/Time/serializers.py
+++ Project/Infrastructure/InsemiSystem/Time/serializers.py
@@ -4,7 +4,7 @@ from .models import *
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        fields = ('id','employeeId','employeeName','Project_code','Project_name', 'Manager','Vendor_name','Leaves_availed','Work_location','Customer_name','Date','Day','Opening_time','Closing_time','Total_hours','Status','Remarks')
+        fields = ('id','employeeId','Project_code','Date','Opening_time','Closing_time','Total_hours','Status','Remarks')
 
 
 class ChoicesSerializer(serializers.ModelSerializer):
